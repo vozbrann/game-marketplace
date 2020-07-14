b64b3148644d964943bed95d3c320c5d6822c6f4
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2020-07-14 12:51:16 +0300

    cross browser scrollbar and input changes

---
+++ assets/js/header.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+  $( document ).click(function(e) {
+    if (!$(e.target).parents(".search-container").length && !$(e.target).parents(".main-header .search-button").length) {
+      $( '.search-container' ).collapse('hide');
+      $('.main-header > div').css('visibility', 'visible');
+    }
+    if (!$(e.target).parents(".notifications-dropdown-menu").length) {
+      $( '.notifications-dropdown-menu' ).collapse('hide');
+    }
+  });
+  $('.main-header .search-button').click(function(e) {
+    $('.main-header > div').css('visibility', 'hidden');
+  })
+});
