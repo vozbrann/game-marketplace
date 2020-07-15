3115a5cf9dd682029ab9c5013c705a4af75e3d24
Roman Vozbrannyi <romanvozbrannyi@gmail.com>
2020-07-15 20:04:28 +0300

    main page localstorag, menu autoscroll, menu active by url, owl sliders

--- assets/js/mainMenu.js
+++ assets/js/mainMenu.js
@@ -1,23 +1,46 @@
-$('.menu-fixed-toggle').click(function() {
-  $('body').toggleClass('collapsed');
-  setTimeout(function() {
-    $('.slick-slider').slick('setPosition');
-  }, 301);
-});
-
 $(document).ready(function(e) {
-  $(".main-menu").overlayScrollbars({
+  var mainMenuCollapsed = localStorage.getItem('menu-collapsed');
+  if (mainMenuCollapsed) {
+    $('body').addClass('collapsed');
+  } else {
+    $('body').removeClass('collapsed');
+  }
+
+  var instance = $(".main-menu").overlayScrollbars({
     overflowBehavior: {
       x: "hidden",
     },
-  });
+  }).overlayScrollbars();
   $("body").overlayScrollbars({});
+  var current = location.pathname;
+  $('.main-menu a.nav-link').each(function(){
+    var $this = $(this);
+    if(current.includes($this.attr('href'))){
+      $this.addClass('active');
+      $(this).closest(".collapse").collapse('show');
+      instance.scroll({ el : $this, block : { y : "center", x : "nearest" } });
+    }
+  })
+});
+
+$('.menu-fixed-toggle').click(function() {
+  $('body').toggleClass('collapsed');
+  var mainMenuCollapsed = localStorage.getItem('menu-collapsed');
+  if (mainMenuCollapsed) {
+    localStorage.setItem('menu-collapsed', "")
+  } else {
+    localStorage.setItem('menu-collapsed', "true")
+  }
+  setTimeout(function() {
+    $('.owl-carousel').trigger('refresh.owl.carousel');
+  }, 301);
 });
 
 $(document).click(function(e) {
   if ($(window).width() < 992 && !$(e.target).closest('.main-menu').length &&
     !$(e.target).closest('.menu-fixed-toggle').length) {
     $('body').removeClass('collapsed');
+    localStorage.setItem('menu-collapsed', "")
   }
 });
 
